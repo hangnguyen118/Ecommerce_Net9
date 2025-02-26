6c1e0097b3e4db988c1b1cdcb20ddf9364317179
hangnguyen118 <hangnguyen118.wy@gmail.com>
2025-02-26 17:19:54 +0700

    first commit

---
+++ RazorWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorWeb.Models;
+
+namespace RazorWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Book 1", Description = "no description" },
+                new Category { Id = 2, Name = "Book 2", Description = "no description" },
+                new Category { Id = 3, Name = "Book 3", Description = "no description" }
+                );
+        }
+    }
+}
